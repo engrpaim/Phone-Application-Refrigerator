36eaf7a0992db4a66e6908d377f4a58f1e52c6ff
Automation Engr <automation3@smp.local>
2024-08-08 09:49:23 +0800

    buttons initial

--- app/src/main/java/com/example/myapplication/Databarcode.kt
+++ app/src/main/java/com/example/myapplication/Databarcode.kt
@@ -1,12 +1,17 @@
 // DataBarcode.kt
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 
 class Databarcode: AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_databarcode)
@@ -19,11 +24,28 @@ class Databarcode: AppCompatActivity() {
 
             if(data.size == 3){
                 val dataNameView = findViewById<TextView>(R.id.dataName) // Example data array
-                dataNameView.text = data[1]
+                dataNameView.text = data[2]
 
                 val idNameView = findViewById<TextView>(R.id.dataID)
-                idNameView.text = data[2]
+                idNameView.text = data[1]
             }
+        }
+
+        val reScanButton: Button = findViewById(R.id.rescanBtn)
+        reScanButton.setOnClickListener {
+
+            val scannerOpener = Intent(this, ScannerActivity::class.java)
+            startActivity(scannerOpener)
+        }
+
+        val conFirmButton: Button = findViewById(R.id.confirmBtn)
+        conFirmButton.setOnClickListener{
+
+            reScanButton.isEnabled = false
+
+
+
+
         }
 
         /*
@@ -32,10 +54,15 @@ class Databarcode: AppCompatActivity() {
 
     }
 
+
+
     override fun onBackPressed() {
         // Do nothing to disable back button
     }
 }
+
+
+
 
 fun mapData(value: String): String {
     return when (value) {
